47f3dd9ae3eaf9685cd478dcb46ba85f5b8d8268
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-11-08 12:15:24 -0600

    Avance de los Test

---
+++ xUnit.Test/Validar_Email.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Validar_Email.Controllers;
+using Validar_Email.Models;
+
+
+namespace xUnit.Test
+{
+    public class Validar_Email
+    {
+
+        [Fact]
+        public void Email_Valido()
+        {
+            // Arange:
+            CredencialesController Controlador = new CredencialesController();
+            Credenciales credenciales = new Credenciales()
+            {
+                Email = "Hola@gmail.com",
+                Password="1234567"
+            };
+
+
+            // Act:
+            var result = Controlador.Registrar(credenciales) as RedirectToActionResult;
+
+
+            // Assert:
+            Assert.Equal("Pagina_Inicio", result.ActionName);
+            Assert.Equal("Credenciales", result.ControllerName);
+        }
+    }
+}
